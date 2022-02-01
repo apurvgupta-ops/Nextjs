c69331dd34f52f701b6cb6accd8f79996b27ba68
apurvgupta-ops <56751664+apurvgupta-ops@users.noreply.github.com>
2022-02-01 13:31:04 +0530

    Navigate Programmatically

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+
 const index = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.replace("/products");
+  };
+
   return (
     <div>
       <Link href="/blog">
         <a>Blog</a>
       </Link>
-      Homepage
+      <div>Homepage</div>
+      <div>
+        <button onClick={handleClick}>Place order</button>
+      </div>
     </div>
   );
 };
