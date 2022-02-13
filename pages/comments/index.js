403bfdd824babdd4d9cffd68f5c4ecf1aa6f2e1e
apurvgupta-ops <56751664+apurvgupta-ops@users.noreply.github.com>
2022-02-13 23:45:49 +0530

    Api routes with POST request

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,15 +2,38 @@ import React, { useState } from "react";
 
 const commentsPage = () => {
   const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
+
   const fetchComments = async () => {
     const response = await fetch("/api/comments");
     const data = await response.json();
     setComments(data);
   };
 
+  const submitComments = async () => {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ comment }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    // setComment("");
+    console.log(data);
+  };
+
   return (
     <>
       <h1>commentsPage</h1>
+      <div>
+        <input
+          type="text"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+        <button onClick={submitComments}>Submit Comments</button>
+      </div>
       <button onClick={fetchComments}>Load Comments</button>
       {comments.map((cm) => {
         return (
